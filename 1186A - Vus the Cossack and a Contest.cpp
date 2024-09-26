c9835954bc954962fec2f89e0d54ed420ae99bbc
Abrar Rakin <144883380+A-Rakin@users.noreply.github.com>
2024-09-26 16:43:54 +0600

    Add files via upload

---
+++ 1186A - Vus the Cossack and a Contest.cpp
@@ -0,0 +1,20 @@
+/*
+Problem Link:
+https://codeforces.com/contest/1186/problem/A
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,p,b;
+    cin>>n>>p>>b;
+    if(p>=n&&b>=n)
+    {
+        cout<<"Yes"<<endl;
+    }
+    else
+    {
+        cout<<"No"<<endl;
+    }
+    return 0;
+}
