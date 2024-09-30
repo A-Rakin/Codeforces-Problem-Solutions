422db50a7920cf1c6b26fa74155a9af63c1d6980
Abrar Rakin <144883380+A-Rakin@users.noreply.github.com>
2024-09-30 21:52:19 +0600

    Add files via upload

---
+++ 50A - Domino Piling.cpp
@@ -0,0 +1,15 @@
+/*
+Problem Link:
+https://codeforces.com/contest/50/problem/A
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double a,b;
+    cin>>a>>b;
+    long long res= ceil (a*b)/2;
+    cout<<res<<endl;
+    return 0;
+} 
