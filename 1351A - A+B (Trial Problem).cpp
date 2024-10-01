b2f5709330c337bd58c25d75609543fca828df00
Abrar Rakin <144883380+A-Rakin@users.noreply.github.com>
2024-10-01 20:58:29 +0600

    Add files via upload

---
+++ 1351A - A+B (Trial Problem).cpp
@@ -0,0 +1,17 @@
+/*
+Problem Link:
+https://codeforces.com/contest/1351/problem/A
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int t;
+  cin>>t;
+  while(t--)
+  {
+      int a,b;
+      cin>>a>>b;
+      cout<<a+b<<endl;
+  }
+}
