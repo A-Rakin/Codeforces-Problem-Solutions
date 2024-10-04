5197d18febbbe6159727351f71bc01676b5c84b7
Abrar Rakin <144883380+A-Rakin@users.noreply.github.com>
2024-10-04 18:31:23 +0600

    Add files via upload

---
+++ 1550A - Find The Array.cpp
@@ -0,0 +1,19 @@
+/*
+Problem Link:
+https://codeforces.com/contest/1550/problem/A
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int s;
+        cin >> s;
+        int p =ceil(sqrt(s));
+        cout <<p <<endl;
+    }
+    return 0;
+}
