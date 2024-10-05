429e8067b2e9d40464faec4414d7e9abf628799b
Abrar Rakin <144883380+A-Rakin@users.noreply.github.com>
2024-10-05 20:39:36 +0600

    Add files via upload

---
+++ 1553A - Digits Sum.cpp
@@ -0,0 +1,17 @@
+/*
+Problem Link:
+https://codeforces.com/contest/1553/problem/A
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >>t;
+    while (t--)
+    {
+        int d;
+        cin>>d;
+        cout<<(d + 1)/10<<endl;
+    }
+}
