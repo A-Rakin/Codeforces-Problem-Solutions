34536dfa03bbbbc92d51e114d09f0ace44019759
Abrar Rakin <144883380+A-Rakin@users.noreply.github.com>
2024-10-07 14:45:47 +0600

    Add files via upload

---
+++ 664A - Coplicated GCD.cpp
@@ -0,0 +1,21 @@
+/*
+Problem Link:
+vhttps://codeforces.com/contest/664/problem/A
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string n,m;
+    cin>>n>>m;
+    if(n==m)
+    {
+       cout<<n<<endl;
+    }
+    else
+    {
+       cout<<1<<endl;
+    }
+
+}
