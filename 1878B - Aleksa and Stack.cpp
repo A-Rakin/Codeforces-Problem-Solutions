63b9e202fff5cc70f3cc20b6b8dc12280b5d8993
Abrar Rakin <144883380+A-Rakin@users.noreply.github.com>
2024-10-12 19:59:26 +0600

    Add files via upload

---
+++ 1878B - Aleksa and Stack.cpp
@@ -0,0 +1,20 @@
+/*
+Problem link:
+https://codeforces.com/contest/1878/problem/B
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        for(int i=1;i<=n;i++)
+        {
+            cout<<i*2+1<<endl;
+        }
+    }
+}
