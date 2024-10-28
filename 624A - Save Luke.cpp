d6d1f4c51138ffb6c1d036153c03bf4e8934f6a7
Abrar Rakin <144883380+A-Rakin@users.noreply.github.com>
2024-10-28 15:21:03 +0600

    Add files via upload

---
+++ 624A - Save Luke.cpp
@@ -0,0 +1,13 @@
+/*
+Problem Link:
+https://codeforces.com/contest/624/problem/A
+*/
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    double d,l,v,b;cin>>d>>l>>v>>b;
+    cout<<setprecision(16);
+    cout<<(l-d)/(v+b)<<endl;
+}
